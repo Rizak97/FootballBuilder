b9301655c640c27f7fe00671705823027fe7a84d
abdirizak osman <abdirizakosman123@gmail.com>
2022-08-18 19:00:16 +0100

    swagger dependency added

--- FootballTeam/src/main/java/com/qa/main/FootballTeamApplication.java
+++ FootballTeam/src/main/java/com/qa/main/FootballTeamApplication.java
@@ -1,9 +1,8 @@
 package com.qa.main;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
+@EnableWebMvc
 @SpringBootApplication
 public class FootballTeamApplication {
 
